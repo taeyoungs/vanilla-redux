d3e4cc0e51b69be8ed7adffcb8928e7e09dbaad1
taeyoung <xoxodudwkd@gmail.com>
2020-05-06 22:50:55 +0900

    actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const span = document.querySelector('span');
 
-const modifyCount = (state = 0) => {
-  return state;
+const modifyCount = (state = 0, action) => {
+  console.log(state, action);
+  if (action.type === 'ADD') {
+    return state + 1;
+  } else if (action.type === 'MINUS') {
+    return state - 1;
+  } else {
+    return state;
+  }
 };
 
 const countStore = createStore(modifyCount);
+
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
 
 console.log(countStore.getState());
