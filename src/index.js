930903e4298b658a89375d4f9bb26dd3fd1cd6f3
taeyoung <xoxodudwkd@gmail.com>
2020-05-09 10:23:56 +0900

    add redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
+import { createStore } from 'redux';
+
 const ul = document.getElementById('list');
 const form = document.querySelector('form');
 const input = document.getElementById('txt');
 
-const addToDo = (value) => {
-  const li = document.createElement('li');
-  li.innerText = value;
-  ul.appendChild(li);
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
+
+const store = createStore(reducer);
 
 const handleSubmit = (event) => {
   event.preventDefault();
   const value = input.value;
-  addToDo(value);
   input.value = '';
+  store.dispatch({ type: ADD_TODO, text: value });
 };
 
 form.addEventListener('submit', handleSubmit);
