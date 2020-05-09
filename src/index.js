f3f504591e985a6eeeb876e22fb753ce1a08376a
taeyoung <xoxodudwkd@gmail.com>
2020-05-09 10:39:56 +0900

    state mutation

--- src/index.js
+++ src/index.js
@@ -7,19 +7,24 @@ const input = document.getElementById('txt');
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
 
+// 주의 !!
+// 절대 기존의 state를 수정하지 말 것
+// 기존의 state + 추가할 obj를 담은 새로운 arr를 return 할 것
 const reducer = (state = [], action) => {
-  console.log(action);
+  // console.log(state);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text, id: Date.now() }];
     case DELETE_TODO:
-      return [];
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
 };
 
 const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 const handleSubmit = (event) => {
   event.preventDefault();
