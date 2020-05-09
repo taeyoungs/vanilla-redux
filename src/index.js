ca619f8abfbd4e2f843ff1d9d2030dd4453d67a2
taeyoung <xoxodudwkd@gmail.com>
2020-05-09 11:02:41 +0900

    delete to do Part 1

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ const input = document.getElementById('txt');
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
 
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 // 주의 !!
 // 절대 기존의 state를 수정하지 말 것
 // 기존의 state + 추가할 obj를 담은 새로운 arr를 return 할 것
@@ -14,9 +28,9 @@ const reducer = (state = [], action) => {
   // console.log(state);
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
-      return state.filter((toDo) => toDo.id !== action.id);
+      return state.filter((toDo) => String(toDo.id) !== action.id);
     default:
       return state;
   }
@@ -26,11 +40,38 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchDeleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
+  toDoPainting();
+};
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const toDoPainting = () => {
+  const toDos = store.getState();
+  ul.innerHTML = '';
+  toDos.forEach((toDo) => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    btn.innerText = 'X';
+    btn.addEventListener('click', dispatchDeleteToDo);
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(toDoPainting);
+
 const handleSubmit = (event) => {
   event.preventDefault();
   const value = input.value;
   input.value = '';
-  store.dispatch({ type: ADD_TODO, text: value });
+  dispatchAddToDo(value);
 };
 
 form.addEventListener('submit', handleSubmit);
