f33e8078e8276b7f5ff9916b4f0b70603dee132e
taeyoung <xoxodudwkd@gmail.com>
2020-05-11 15:47:49 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 // -- Redux Toolkit 사용 전
 // const ADD = 'ADD';
@@ -25,21 +25,34 @@ const deleteToDo = createAction('DELETE');
 
 // console.log(addToDo(), deleteToDo());
 
+// -- Redux Toolkit 사용 전
+
 // reducer에서 return 하는 값이 state의 새로운 값으로 대체됨
-const reducer = (
-  state = JSON.parse(localStorage.getItem('toDos')) || [],
-  action,
-) => {
-  // console.log(localStorage.getItem('toDos'));
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+
+// const reducer = (
+//   state = JSON.parse(localStorage.getItem('toDos')) || [],
+//   action,
+// ) => {
+//   // console.log(localStorage.getItem('toDos'));
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+// -- Redux Toolkit 사용 후
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
 const store = createStore(reducer);
 
